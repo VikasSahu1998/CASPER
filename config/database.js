79fcc1a961331dc1f8d5b418531409c24acfd4c1
VikasSahu1998 <sahuvikas1998@gmail.com>
2024-08-22 15:40:34 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,28 +1,28 @@
 
-// const {Sequelize} = require("sequelize");
-// const sequelize = new Sequelize(
-//     "NOCAS_database",
-//     "postgres",
-//     "postgres",
-//     {
-//         host: "mydb.cxeos2wmmsqf.us-east-2.rds.amazonaws.com",
-//         dialect: "postgres",
-//         port: 5432 
-//     }
-// );
- 
-// module.exports = sequelize;
- 
- 
- 
 const {Sequelize} = require("sequelize");
-const sequelize =new Sequelize(
-    "backend",
-    "test",
-    "password",
+const sequelize = new Sequelize(
+    "NOCAS_database",
+    "postgres",
+    "postgres",
     {
-        host : "localhost",
+        host: "mydb.cxeos2wmmsqf.us-east-2.rds.amazonaws.com",
         dialect: "postgres",
+        port: 5432 
     }
 );
+ 
 module.exports = sequelize;
+ 
+ 
+ 
+// const {Sequelize} = require("sequelize");
+// const sequelize =new Sequelize(
+//     "backend",
+//     "test",
+//     "password",
+//     {
+//         host : "localhost",
+//         dialect: "postgres",
+//     }
+// );
+// module.exports = sequelize;
